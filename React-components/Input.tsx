26cdd3592eda6aa549a471293ef04580fc416208
HarshK200 <harshkapadia04@gmail.com>
2025-02-01 23:08:57 +0530

    added some react components

---
+++ React-components/Input.tsx
@@ -0,0 +1,20 @@
+type tInput = {
+  name?: string;
+  placeholder?: string;
+  classname?: string;
+  type: React.HTMLInputTypeAttribute;
+};
+
+const Input = ({ name, placeholder, type, classname }: tInput) => {
+  return (
+    <input
+      name={name}
+      placeholder={placeholder}
+      type={type}
+      className={`flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm ${classname}`}
+    />
+  );
+};
+
+export { Input };
+
