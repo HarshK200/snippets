c93390f208ab496e6862af664c14594d867f98a1
HarshK200 <harshkapadia04@gmail.com>
2025-03-26 17:09:52 +0530

    better colors for input component

--- React-components/ui/Input.tsx
+++ React-components/ui/Input.tsx
@@ -7,7 +7,7 @@ export const Input = ({ label, id, className, ...props }: InputProps) => {
     <input
       id={id}
       className={cn(
-        "w-full h-fit rounded-md px-3 py-1 border-[1px] focus:outline-none",
+        "w-full h-fit text-sm rounded-md shadow-xs px-3 py-1.5 border-[1px] border-gray-300 focus:border-gray-400 focus:outline-none",
         className,
       )}
       {...props}
