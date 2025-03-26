d675f08c70aa77906aaf20bc8725b3eaff5d6dc6
HarshK200 <harshkapadia04@gmail.com>
2025-03-26 16:48:24 +0530

    fixed the input outline issue

--- React-components/ui/Input.tsx
+++ React-components/ui/Input.tsx
@@ -7,7 +7,7 @@ export const Input = ({ label, id, className, ...props }: InputProps) => {
     <input
       id={id}
       className={cn(
-        "w-full h-fit rounded-md px-3 py-1 border-[1px]",
+        "w-full h-fit rounded-md px-3 py-1 border-[1px] focus:outline-none",
         className,
       )}
       {...props}
